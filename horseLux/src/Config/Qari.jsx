f57c3b8cf7d3a6ab67b075946475862c802c9633
junaid-944 <junaidurrahman840@gmail.com>
2023-08-05 00:56:31 +0500

    change

---
+++ horseLux/src/Config/Qari.jsx
@@ -0,0 +1,88 @@
+import img from "../images/horse-head.png";
+import img1 from "../images/heart.png";
+import img2 from "../images/services.png";
+import img3 from "../images/renewable.png";
+import img4 from "../images/edit.png";
+import img5 from "../images/thermometer.png";
+import img6 from "../images/Beehive.png";
+import img8 from "../images/Contacts.png";
+import img9 from "../images/Upload.png";
+import img10 from "../images/User Groups.png";
+import img11 from "../images/Clock.png";
+import img12 from "../images/Appointment Time.png";
+import img13 from "../images/To Do.png";
+import img14 from "../images/Receive Dollar.png";
+
+
+const qari = [
+  {
+    img: img,
+    text: "Horses",
+  },
+  {
+    img: img1,
+    text: "Heart",
+  },
+  {
+    img: img2,
+    text: "Services",
+  },
+  {
+    img: img3,
+    text: "Renewal",
+  },
+  {
+    img: img4,
+    text: "Notes",
+  },
+  {
+    img: img5,
+    text: "Temperature",
+  },
+  {
+    img: img6,
+    text: "Breeding",
+  },
+
+
+];
+
+export default qari;
+
+export const aziz = [
+  {
+    img: img8,
+    text: "Contact",
+  },
+  {
+    img: img9,
+    text: "Upload",
+  },
+  {
+    img: img10,
+    text: "Owner Group",
+  },
+];
+
+
+export const junaid = [
+    {
+      img: img11,
+      text: "Service",
+    },
+    {
+      img: img12,
+      text: "Appointment",
+    },
+    {
+      img: img13,
+      text: "Task",
+    },
+  ];
+
+  export const asad = [
+    {
+      img: img14,
+      text: "Payment Recieved",
+    },
+  ];
