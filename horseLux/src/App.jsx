5ff194d1841073f565105fb0b0039d6c2e8dcffb
junaid-944 <junaidurrahman840@gmail.com>
2023-08-04 12:28:18 +0500

    change

--- horseLux/src/App.jsx
+++ horseLux/src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Home'
+// import QariCard from './Pages/QariCard'
+import Card from './Pages/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Home/>
-     
+    {/* <Home/> */}
+     <Card/>
     </>
   )
 }
