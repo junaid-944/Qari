5ae1197f7add21c4fb0021896a7a42730a23fe49
Azizhere <azizthefalcon@gmail.com>
2023-08-04 05:54:20 -0700

    CHANGE

--- horseLux/src/App.jsx
+++ horseLux/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Pages/Home'
+import Aziz from './Pages/Aziz'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Home/>
+    {/* <Home/> */}
+    <Aziz/>
      
     </>
   )
